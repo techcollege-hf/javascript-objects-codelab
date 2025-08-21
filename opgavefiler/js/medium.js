2d4da6357ed896c2c21e7d939d1887f0d088f6f9
bo-nicolaisen <bo@squirrelworks.dk>
2025-08-21 12:57:15 +0200

    done

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -1,8 +1,7 @@
 /* Opgave 1*/
 console.group("opgave 1");
 
-let myBall = document.getElementById('ballOne');
-let myBuhButton = document.getElementById('buhOneButton');
+
 
 console.groupEnd();
 
@@ -11,8 +10,7 @@ console.groupEnd();
 /*opgave 2*/
 console.group("opgave 2");
 
-let myBalls = document.getElementsByClassName('balls');
-let myBuhTwoButton = document.getElementById('buhTwoButton');
+
 
 console.groupEnd();
 
